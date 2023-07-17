592e81faac6ec00fe04a73873f48cb7a0eca8e66
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-07-18 01:19:13 +0300

    Added tips about the units of intoxication from alcoholic beverages

---
+++ src/main/java/power/forestxreborn/procedures/WineAlcoholDescProcedure.java
@@ -0,0 +1,9 @@
+package power.forestxreborn.procedures;
+
+import net.minecraft.network.chat.Component;
+
+public class WineAlcoholDescProcedure {
+	public static String execute() {
+		return "\u00A7c+0.3 " + Component.translatable("tooltip.translation.intoxication").getString();
+	}
+}
