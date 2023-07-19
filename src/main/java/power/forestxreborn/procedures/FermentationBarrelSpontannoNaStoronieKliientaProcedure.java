2fc8469fd1001b2b43f0c4237899702ec780edc5
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-07-19 20:28:54 +0300

    Refactor fermentation barrel code

--- src/main/java/power/forestxreborn/procedures/FermentationBarrelSpontannoNaStoronieKliientaProcedure.java
+++ src/main/java/power/forestxreborn/procedures/FermentationBarrelSpontannoNaStoronieKliientaProcedure.java
@@ -5,13 +5,18 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.BlockPos;
 
 public class FermentationBarrelSpontannoNaStoronieKliientaProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		double fireHeight = 0;
+		double particleRadius = 0;
+		double particleAmount = 0;
 		if (new Object() {
 			public double getValue(LevelAccessor world, BlockPos pos, String tag) {
 				BlockEntity blockEntity = world.getBlockEntity(pos);
@@ -26,6 +31,32 @@ public class FermentationBarrelSpontannoNaStoronieKliientaProcedure {
 						_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.brewing_stand.brew")), SoundSource.BLOCKS, 1, 1);
 					} else {
 						_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.brewing_stand.brew")), SoundSource.BLOCKS, 1, 1, false);
+					}
+				}
+			}
+		} else if (new Object() {
+			public double getValue(LevelAccessor world, BlockPos pos, String tag) {
+				BlockEntity blockEntity = world.getBlockEntity(pos);
+				if (blockEntity != null)
+					return blockEntity.getPersistentData().getDouble(tag);
+				return -1;
+			}
+		}.getValue(world, BlockPos.containing(x, y, z), "fermentation") == 0) {
+			if (new Object() {
+				public double getValue(LevelAccessor world, BlockPos pos, String tag) {
+					BlockEntity blockEntity = world.getBlockEntity(pos);
+					if (blockEntity != null)
+						return blockEntity.getPersistentData().getDouble(tag);
+					return -1;
+				}
+			}.getValue(world, BlockPos.containing(x, y, z), "recipe") >= 1) {
+				if (Math.random() < 0.2) {
+					particleAmount = 4;
+					particleRadius = 0.5;
+					for (int index0 = 0; index0 < (int) particleAmount; index0++) {
+						world.addParticle(ParticleTypes.DRAGON_BREATH, (x + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), (y + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius),
+								(z + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)), (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)),
+								(Mth.nextDouble(RandomSource.create(), -0.001, 0.001)));
 					}
 				}
 			}
