f8271d64f64487d89f243372f899f25a91cef001
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-08-01 13:37:53 +0300

    Added new painting - Pig in mood

--- src/main/java/power/forestxreborn/init/ForestModPaintings.java
+++ src/main/java/power/forestxreborn/init/ForestModPaintings.java
@@ -15,4 +15,5 @@ import net.minecraft.world.entity.decoration.PaintingVariant;
 public class ForestModPaintings {
 	public static final DeferredRegister<PaintingVariant> REGISTRY = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, ForestMod.MODID);
 	public static final RegistryObject<PaintingVariant> PAINTING_VOLCANO = REGISTRY.register("painting_volcano", () -> new PaintingVariant(32, 32));
+	public static final RegistryObject<PaintingVariant> PAINTING_PIG_IN_MOOD = REGISTRY.register("painting_pig_in_mood", () -> new PaintingVariant(32, 32));
 }
