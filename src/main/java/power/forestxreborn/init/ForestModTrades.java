384a372b757565a5443cd2ad4bdd268c03353f2f
ShadowMoon02 <69478092+ShadowMoon02@users.noreply.github.com>
2023-08-13 15:16:47 +0300

    Added more trades for villagers with items from the mod

--- src/main/java/power/forestxreborn/init/ForestModTrades.java
+++ src/main/java/power/forestxreborn/init/ForestModTrades.java
@@ -21,9 +21,40 @@ public class ForestModTrades {
 			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3),
 
 					new ItemStack(ForestModItems.LETTUCE_SEED.get(), 16), 10, 5, 0.05f));
-			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 4),
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 4),
 
 					new ItemStack(ForestModBlocks.GRAPE_VINE_0.get()), 10, 5, 0.05f));
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(ForestModItems.GRAPE.get(), 12),
+
+					new ItemStack(Items.EMERALD), 10, 5, 0.05f));
+		}
+		if (event.getType() == VillagerProfession.ARMORER) {
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 8),
+
+					new ItemStack(ForestModItems.SILVER_ARMOR_HELMET.get()), 10, 5, 0.15f));
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 14),
+
+					new ItemStack(ForestModItems.SILVER_ARMOR_CHESTPLATE.get()), 10, 5, 0.15f));
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 11),
+
+					new ItemStack(ForestModItems.SILVER_ARMOR_LEGGINGS.get()), 10, 5, 0.15f));
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 8),
+
+					new ItemStack(ForestModItems.SILVER_ARMOR_BOOTS.get()), 10, 5, 0.15f));
+		}
+		if (event.getType() == VillagerProfession.WEAPONSMITH) {
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5),
+
+					new ItemStack(ForestModItems.SILVER_SWORD.get()), 10, 5, 0.15f));
+			event.getTrades().get(4).add(new BasicItemListing(new ItemStack(ForestModItems.SILVER_INGOT.get(), 8),
+
+					new ItemStack(Items.EMERALD), 10, 5, 0.05f));
+		}
+		if (event.getType() == VillagerProfession.FLETCHER) {
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Items.EMERALD, 7), new ItemStack(Items.LEATHER, 2), new ItemStack(ForestModItems.QUIVER.get()), 10, 5, 0.05f));
+			event.getTrades().get(4).add(new BasicItemListing(new ItemStack(Items.EMERALD, 11),
+
+					new ItemStack(ForestModItems.DART.get(), 8), 10, 5, 0.05f));
 		}
 	}
 }
