baab3689e22e93e509c65208f3be3e8d8863c8d1
ShadowMoon02 <69478092+ShadowMoon02@users.noreply.github.com>
2023-08-13 14:25:20 +0300

    Fix Sarbakan

--- src/main/java/power/forestxreborn/client/renderer/SarbakanRenderer.java
+++ src/main/java/power/forestxreborn/client/renderer/SarbakanRenderer.java
@@ -27,8 +27,6 @@ public class SarbakanRenderer extends EntityRenderer<SarbakanEntity> {
 	public void render(SarbakanEntity entityIn, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource bufferIn, int packedLightIn) {
 		VertexConsumer vb = bufferIn.getBuffer(RenderType.entityCutout(this.getTextureLocation(entityIn)));
 		poseStack.pushPose();
-		poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partialTicks, entityIn.yRotO, entityIn.getYRot()) - 90));
-		poseStack.mulPose(Axis.ZP.rotationDegrees(90 + Mth.lerp(partialTicks, entityIn.xRotO, entityIn.getXRot())));
 		model.renderToBuffer(poseStack, vb, packedLightIn, OverlayTexture.NO_OVERLAY, 1, 1, 1, 0.0625f);
 		poseStack.popPose();
 		super.render(entityIn, entityYaw, partialTicks, poseStack, bufferIn, packedLightIn);
