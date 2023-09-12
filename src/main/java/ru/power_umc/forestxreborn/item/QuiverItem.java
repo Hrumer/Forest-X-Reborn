2304fe31ce59e24c4f452abc048ec913b77c04d5
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-09-12 11:13:06 +0300

    More fixes + improved black mulberry texture

--- src/main/java/ru/power_umc/forestxreborn/item/QuiverItem.java
+++ src/main/java/ru/power_umc/forestxreborn/item/QuiverItem.java
@@ -97,9 +97,8 @@ public class QuiverItem extends Item {
 
 	@Override
 	public CompoundTag getShareTag(ItemStack stack) {
-		CompoundTag nbt = super.getShareTag(stack);
-		if (nbt != null)
-			stack.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(capability -> nbt.put("Inventory", ((ItemStackHandler) capability).serializeNBT()));
+		CompoundTag nbt = stack.getOrCreateTag();
+		stack.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(capability -> nbt.put("Inventory", ((ItemStackHandler) capability).serializeNBT()));
 		return nbt;
 	}
 
