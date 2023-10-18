e3d2d425923d79a98604065bee022461d70c3d06
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-10-18 20:13:39 +0300

    Fixed placements

--- src/main/java/ru/power_umc/forestxreborn/procedures/BlueberriesBushFeatureAdditionalGenerationConditionProcedure.java
+++ src/main/java/ru/power_umc/forestxreborn/procedures/BlueberriesBushFeatureAdditionalGenerationConditionProcedure.java
@@ -1,13 +1,11 @@
 package ru.power_umc.forestxreborn.procedures;
 
-import net.minecraftforge.registries.ForgeRegistries;
-
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
 public class BlueberriesBushFeatureAdditionalGenerationConditionProcedure {
 	public static boolean execute(LevelAccessor world, double x, double y, double z) {
-		return (world.getBlockState(BlockPos.containing(x, y - 1, z))).getBlock() == ForgeRegistries.BLOCKS.getValue(new ResourceLocation("minecraft:dirt"));
+		return (world.getBlockState(BlockPos.containing(x, y - 1, z))).getBlock() == Blocks.GRASS_BLOCK;
 	}
 }
