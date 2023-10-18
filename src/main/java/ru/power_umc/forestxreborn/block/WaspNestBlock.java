c0c2353dd65f4caefe88da1aa877c588cb68105a
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-10-18 21:32:11 +0300

    Added wasp nest

---
+++ src/main/java/ru/power_umc/forestxreborn/block/WaspNestBlock.java
@@ -0,0 +1,35 @@
+
+package ru.power_umc.forestxreborn.block;
+
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.BlockPos;
+
+import java.util.List;
+import java.util.Collections;
+
+public class WaspNestBlock extends Block {
+	public WaspNestBlock() {
+		super(BlockBehaviour.Properties.of().ignitedByLava().instrument(NoteBlockInstrument.BASS).sound(SoundType.WOOD).strength(1f, 10f));
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 15;
+	}
+
+	@Override
+	public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
+		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+		if (!dropsOriginal.isEmpty())
+			return dropsOriginal;
+		return Collections.singletonList(new ItemStack(Blocks.AIR));
+	}
+}
--- src/main/java/ru/power_umc/forestxreborn/init/ForestModTabs.java
+++ src/main/java/ru/power_umc/forestxreborn/init/ForestModTabs.java
@@ -56,6 +56,7 @@ public class ForestModTabs {
 				tabData.accept(ForestModBlocks.EUCALYPTUS_TRAPDOOR.get().asItem());
 				tabData.accept(ForestModBlocks.EUCALYPTUS_PRESSURE_PLATE.get().asItem());
 				tabData.accept(ForestModBlocks.EUCALYPTUS_BUTTON.get().asItem());
+				tabData.accept(ForestModBlocks.WASP_NEST.get().asItem());
 			})
 
 					.build());
