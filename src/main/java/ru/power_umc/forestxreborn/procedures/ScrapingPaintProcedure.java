146cf664fc96a70d5c4103fae711aa0c1ceb3035
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-10-22 13:55:26 +0300

    Fixed a bug with the third sound ability

--- src/main/java/ru/power_umc/forestxreborn/procedures/ScrapingPaintProcedure.java
+++ src/main/java/ru/power_umc/forestxreborn/procedures/ScrapingPaintProcedure.java
@@ -55,68 +55,59 @@ public class ScrapingPaintProcedure {
 					}
 				}
 			}
-			if (!ForgeRegistries.BLOCKS.getValue(new ResourceLocation(((new Object() {
+			if (!(new Object() {
 				public String getValue(LevelAccessor world, BlockPos pos, String tag) {
 					BlockEntity blockEntity = world.getBlockEntity(pos);
 					if (blockEntity != null)
 						return blockEntity.getPersistentData().getString(tag);
 					return "";
 				}
-			}.getValue(world, BlockPos.containing(x, y, z), "defaultMaterial"))).toLowerCase(java.util.Locale.ENGLISH))).defaultBlockState().is(BlockTags.create(new ResourceLocation("minecraft:painted_blocks")))) {
-				if (!(new Object() {
-					public String getValue(LevelAccessor world, BlockPos pos, String tag) {
-						BlockEntity blockEntity = world.getBlockEntity(pos);
-						if (blockEntity != null)
-							return blockEntity.getPersistentData().getString(tag);
-						return "";
+			}.getValue(world, BlockPos.containing(x, y, z), "defaultMaterial")).isEmpty()) {
+				if (world instanceof Level _level) {
+					if (!_level.isClientSide()) {
+						_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.axe.strip")), SoundSource.NEUTRAL, 1, 1);
+					} else {
+						_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.axe.strip")), SoundSource.NEUTRAL, 1, 1, false);
 					}
-				}.getValue(world, BlockPos.containing(x, y, z), "defaultMaterial")).isEmpty()) {
-					if (world instanceof Level _level) {
-						if (!_level.isClientSide()) {
-							_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.axe.strip")), SoundSource.NEUTRAL, 1, 1);
-						} else {
-							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.axe.strip")), SoundSource.NEUTRAL, 1, 1, false);
-						}
-					}
-					{
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockState _bs = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(((new Object() {
-							public String getValue(LevelAccessor world, BlockPos pos, String tag) {
-								BlockEntity blockEntity = world.getBlockEntity(pos);
-								if (blockEntity != null)
-									return blockEntity.getPersistentData().getString(tag);
-								return "";
-							}
-						}.getValue(world, BlockPos.containing(x, y, z), "defaultMaterial"))).toLowerCase(java.util.Locale.ENGLISH))).defaultBlockState();
-						BlockState _bso = world.getBlockState(_bp);
-						for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-							Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-							if (_property != null && _bs.getValue(_property) != null)
-								try {
-									_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-								} catch (Exception e) {
-								}
-						}
-						BlockEntity _be = world.getBlockEntity(_bp);
-						CompoundTag _bnbt = null;
-						if (_be != null) {
-							_bnbt = _be.saveWithFullMetadata();
-							_be.setRemoved();
-						}
-						world.setBlock(_bp, _bs, 3);
-						if (_bnbt != null) {
-							_be = world.getBlockEntity(_bp);
-							if (_be != null) {
-								try {
-									_be.load(_bnbt);
-								} catch (Exception ignored) {
-								}
-							}
-						}
-					}
-					if (entity instanceof LivingEntity _entity)
-						_entity.swing(InteractionHand.MAIN_HAND, true);
 				}
+				{
+					BlockPos _bp = BlockPos.containing(x, y, z);
+					BlockState _bs = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(((new Object() {
+						public String getValue(LevelAccessor world, BlockPos pos, String tag) {
+							BlockEntity blockEntity = world.getBlockEntity(pos);
+							if (blockEntity != null)
+								return blockEntity.getPersistentData().getString(tag);
+							return "";
+						}
+					}.getValue(world, BlockPos.containing(x, y, z), "defaultMaterial"))).toLowerCase(java.util.Locale.ENGLISH))).defaultBlockState();
+					BlockState _bso = world.getBlockState(_bp);
+					for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+						Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
+						if (_property != null && _bs.getValue(_property) != null)
+							try {
+								_bs = _bs.setValue(_property, (Comparable) entry.getValue());
+							} catch (Exception e) {
+							}
+					}
+					BlockEntity _be = world.getBlockEntity(_bp);
+					CompoundTag _bnbt = null;
+					if (_be != null) {
+						_bnbt = _be.saveWithFullMetadata();
+						_be.setRemoved();
+					}
+					world.setBlock(_bp, _bs, 3);
+					if (_bnbt != null) {
+						_be = world.getBlockEntity(_bp);
+						if (_be != null) {
+							try {
+								_be.load(_bnbt);
+							} catch (Exception ignored) {
+							}
+						}
+					}
+				}
+				if (entity instanceof LivingEntity _entity)
+					_entity.swing(InteractionHand.MAIN_HAND, true);
 			}
 		} else {
 			if (world instanceof Level _level)
