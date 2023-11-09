32bf3a8a02b3fbc892bf70bf3c65d014f7ff32b1
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-11-09 21:50:36 +0300

    Added some new mud blocks

--- src/main/java/ru/power_umc/forestxreborn/init/ForestModItems.java
+++ src/main/java/ru/power_umc/forestxreborn/init/ForestModItems.java
@@ -375,6 +375,8 @@ public class ForestModItems {
 	public static final RegistryObject<Item> RYE_STAGE_5 = block(ForestModBlocks.RYE_STAGE_5);
 	public static final RegistryObject<Item> RYE_STAGE_6 = block(ForestModBlocks.RYE_STAGE_6);
 	public static final RegistryObject<Item> RYE_STAGE_7 = block(ForestModBlocks.RYE_STAGE_7);
+	public static final RegistryObject<Item> CUT_MUD = block(ForestModBlocks.CUT_MUD);
+	public static final RegistryObject<Item> MUD_BRICKS = block(ForestModBlocks.MUD_BRICKS);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties()));
