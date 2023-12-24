11e9e0cc6447a97e38a56660ccd6c727c6987e4d
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-12-24 23:03:00 +0300

    Some fixes

--- src/main/java/ru/power_umc/forestxreborn/block/OxidizedCopperLadderBlock.java
+++ src/main/java/ru/power_umc/forestxreborn/block/OxidizedCopperLadderBlock.java
@@ -27,10 +27,12 @@ import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
@@ -44,6 +46,11 @@ public class OxidizedCopperLadderBlock extends Block implements SimpleWaterlogge
 	public OxidizedCopperLadderBlock() {
 		super(BlockBehaviour.Properties.of().sound(SoundType.COPPER).strength(2f, 10f).requiresCorrectToolForDrops().noCollission().noOcclusion().pushReaction(PushReaction.DESTROY).isRedstoneConductor((bs, br, bp) -> false));
 		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(WATERLOGGED, false));
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, BlockGetter world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
 	}
 
 	@Override
