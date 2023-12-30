b1cdecbcb564b5c0ed2a42b959e3bd550bac0821
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-12-30 18:54:01 +0300

    Fixed the disappearance of foliage with mulberry berries during harvesting (try 2)

--- src/main/java/ru/power_umc/forestxreborn/procedures/MulberryLeavesWithBlackPriShchielchkiePKMPoBlokuProcedure.java
+++ src/main/java/ru/power_umc/forestxreborn/procedures/MulberryLeavesWithBlackPriShchielchkiePKMPoBlokuProcedure.java
@@ -5,6 +5,8 @@ import ru.power_umc.forestxreborn.init.ForestModBlocks;
 
 import net.minecraftforge.registries.ForgeRegistries;
 
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
@@ -14,9 +16,24 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
+import java.util.Map;
+
 public class MulberryLeavesWithBlackPriShchielchkiePKMPoBlokuProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		world.setBlock(BlockPos.containing(x, y, z), ForestModBlocks.MULBERRY_LEAVES_WITHOUT_BLACK.get().defaultBlockState(), 3);
+		{
+			BlockPos _bp = BlockPos.containing(x, y, z);
+			BlockState _bs = ForestModBlocks.MULBERRY_LEAVES_WITHOUT_BLACK.get().defaultBlockState();
+			BlockState _bso = world.getBlockState(_bp);
+			for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+				Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
+				if (_property != null && _bs.getValue(_property) != null)
+					try {
+						_bs = _bs.setValue(_property, (Comparable) entry.getValue());
+					} catch (Exception e) {
+					}
+			}
+			world.setBlock(_bp, _bs, 3);
+		}
 		if (world instanceof Level _level) {
 			if (!_level.isClientSide()) {
 				_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.sweet_berry_bush.pick_berries")), SoundSource.NEUTRAL, 1, 1);
