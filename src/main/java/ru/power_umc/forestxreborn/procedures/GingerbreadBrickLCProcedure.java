e3162bff443821e551f83cb7170e317cb0c07b72
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-12-30 18:40:19 +0300

    Some improvements

--- src/main/java/ru/power_umc/forestxreborn/procedures/GingerbreadBrickLCProcedure.java
+++ src/main/java/ru/power_umc/forestxreborn/procedures/GingerbreadBrickLCProcedure.java
@@ -4,6 +4,6 @@ import net.minecraft.network.chat.Component;
 
 public class GingerbreadBrickLCProcedure {
 	public static String execute() {
-		return Component.translatable("desc.gingerbread_brick").getString();
+		return "\u00A78" + Component.translatable("desc.gingerbread_brick").getString();
 	}
 }
--- src/main/java/ru/power_umc/forestxreborn/procedures/GingerbreadRoundLCProcedure.java
+++ src/main/java/ru/power_umc/forestxreborn/procedures/GingerbreadRoundLCProcedure.java
@@ -4,6 +4,6 @@ import net.minecraft.network.chat.Component;
 
 public class GingerbreadRoundLCProcedure {
 	public static String execute() {
-		return Component.translatable("desc.gingerbread_round").getString();
+		return "\u00A78" + Component.translatable("desc.gingerbread_round").getString();
 	}
 }
